412744d3a3372b0affa44775d165daa18a35fbf6
adrianovaladar <adriano.valadar@outlook.com>
2022-09-17 11:44:42 +0100

    add drawMap, printLine and printColumns. Todo: check behaviour when x and y are different

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
-#include <stdio>
+#include <iostream>
+
+void printLine(const int &x, const char & symbol) {
+    for (int i {}; i < x ; i++) {
+        std::cout << symbol;
+    }
+    std::cout << std::endl;
+}
+
+void printColumns(const int &x, const int &y, const char &symbol) {
+    for(int j {}; j < y ; j++) {
+        if (j == 0 || j == y - 1)
+            std::cout << symbol;
+        else
+            std:: cout << " ";
+    }
+    std::cout << std::endl;
+}
+
+void drawMap(const int &x, const int &y, const char &symbol) {
+    std::cout << "Snake game" << std::endl;
+    printLine(x, symbol);
+    for(int j {}; j < y ; j++)
+        printColumns(x, y, symbol);
+    printLine(x, symbol);
+}
+
 
 int main () {
-    std::cout << "Snake game" << std::endl
+    drawMap(30, 30, '#');
     return 0;
 }
