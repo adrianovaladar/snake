1dabec2d071374f7faa46d4feff1ad5fae818359
adrianovaladar <adriano.valadar@outlook.com>
2022-09-22 22:53:33 +0100

    add kbhit and morePortable functions. may not work on windows

--- Map.cpp
+++ Map.cpp
@@ -4,6 +4,7 @@
 #include<bits/stdc++.h>
 #include <chrono>
 #include <thread>
+#include <termios.h>
 
 
 void Map::printHorizontalFence() {
@@ -30,8 +31,44 @@ void Map::printVerticalFence(int j) {
 
 }
 
+/* http://www.flipcode.com/archives/_kbhit_for_Linux.shtml */
+int morePortable() {
+    struct timeval timeout;
+    fd_set rdset;
+
+    FD_ZERO(&rdset);
+    FD_SET(STDIN_FILENO, &rdset);
+    timeout.tv_sec  = 0;
+    timeout.tv_usec = 0;
+
+    return select(STDOUT_FILENO, &rdset, NULL, NULL, &timeout);
+}
+
+int _kbhit(void) {
+    static int initialized = 0;
+
+    if (! initialized) {
+        // Use termios to turn off line buffering
+        struct termios term;
+        tcgetattr(STDIN_FILENO, &term);
+        term.c_lflag &= ~ICANON;
+        tcsetattr(STDIN_FILENO, TCSANOW, &term);
+        setbuf(stdin, NULL);
+        initialized = 1;
+    }
+
+#if 0
+    int bytesWaiting;
+    ioctl(STDIN_FILENO, FIONREAD, &bytesWaiting);
+    return bytesWaiting;
+#else
+    return morePortable();
+#endif
+}
+
 void Map::drawMap() {
     bool gameOver = false;
+    char c = 'r';
     while(!gameOver) {
         std::cout << "\033[2J\033[1;1H";
         std::cout << "Snake game" << std::endl;
@@ -41,7 +78,9 @@ void Map::drawMap() {
         }
         printHorizontalFence();
         std::this_thread::sleep_for(std::chrono::nanoseconds(1000000000));
-        snake.move('r');
+           if(_kbhit())
+               c = getchar();
+        snake.move(c);
     }
 }
 
