5c715b146270d2b92436316596a30d6911e98f8b
adrianovaladar <adriano.valadar@outlook.com>
2022-09-28 14:32:16 +0100

    add method isGameOver and make some Game methods private

--- Game.cpp
+++ Game.cpp
@@ -36,6 +36,22 @@ void Game::printVerticalFenceAndPlayableArea(int j) {
 
 }
 
+bool Game::isGameOver() {
+    std::vector<std::pair<int, int>> snakePositions = snake.getPositions();
+    bool selfCollision {};
+    selfCollision = std::count(snakePositions.begin(), snakePositions.end(), snake.getPositions().at(0)) > 1 ? true : false;
+    bool fenceCollision {};
+    auto it = snake.getPositions().begin();
+    while (it != snake.getPositions().end()) {
+        if ((*it).first < 0 || (*it).second < 0 || (*it).first > this->x || (*it).second > this->y) {
+            fenceCollision = true;
+            break;
+        }
+        it++;
+    }
+    return selfCollision || fenceCollision;
+}
+
 void Game::init(int i, int j, char symbolFence, char symbolSnake, char symbolFood) {
     this->x = i;
     this->y = j;
@@ -45,10 +61,9 @@ void Game::init(int i, int j, char symbolFence, char symbolSnake, char symbolFoo
 }
 
 void Game::printMap() {
-    bool gameOver = false;
     char c = 'r';
     food.setPosition(std::make_pair(x, y), snake.getPositions());
-    while(!gameOver) {
+    while(!isGameOver()) {
         #ifdef _WIN32
         system("cls");
         #else
