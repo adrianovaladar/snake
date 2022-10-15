16f0b662d30fe56ba756f1a6e72e8baf494eaebb
rogerioLopes <roger_rrl@hotmail.com>
2022-10-15 18:41:39 +0100

    add BestScores class (changes in Game.h)

--- Game.h
+++ Game.h
@@ -4,26 +4,23 @@
 #include "Snake.h"
 #include "Food.h"
 #include "Player.h"
+#include "BestScores.h"
 
 
 class Game {
 private:
     std::pair<int, int> size;
-    char symbol;
-    Snake snake;
     Food food;
+    Snake snake;
+    BestScores bestScores;
+    char symbol;
     int score;
-    const int sizeBestScores;
     void printVerticalFenceAndPlayableArea(int y);
     void printHorizontalFence();
     void readDirectionAndMoveSnake();
-    bool isBestScore(std::vector<Player> players);
-    void writeBestScore(std::vector<Player> players);
-    std::vector<Player> readBestScores();
-    void printBestScores(const std::vector<Player> &players);
     bool isEatFood();
 public:
-    Game() : size(0 ,0), symbol(0), score(0), sizeBestScores(5) {};
+    Game() : size(0 ,0), symbol(0), score(0) {};
     virtual ~Game();
     bool isGameOver();
     bool init(int i, int j, char symbolFence, char symbolSnake, char symbolFood);
