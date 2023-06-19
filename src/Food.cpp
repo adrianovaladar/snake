8e8b8b939ce871e8b10c472eaeaa959f44675d9e
Adriano Valadar <60299267+adrianovaladar@users.noreply.github.com>
2023-06-19 23:38:23 +0100

    refactor Food.cpp

--- src/Food.cpp
+++ src/Food.cpp
@@ -1,11 +1,8 @@
 #include "Food.h"
-#include <algorithm>
-#include <cstdlib>
-#include <ctime>
+#include <random>
 #include <vector>
 
-
-Food::Food() {
+Food::Food() : symbol{'f'} {
     position = std::make_pair(0, 0);
 }
 
@@ -29,10 +26,13 @@ void Food::setPosition(const std::pair<int, int> &sizeMap, const std::vector<std
     std::vector<std::pair<int, int>> posSnake = positionsSnake;
     std::sort(posSnake.begin(), posSnake.end());
     bool isSnakePosition;
+    std::random_device dev;
+    std::mt19937 rng(dev());
     do {
-        srand(time(nullptr));
-        foodPosition.first = std::rand() % sizeMap.first;
-        foodPosition.second = std::rand() % sizeMap.second;
+        std::uniform_int_distribution<int> distX(0, sizeMap.first - 1);
+        std::uniform_int_distribution<int> distY(0, sizeMap.second - 1);
+        foodPosition.first = distX(rng);
+        foodPosition.second = distY(rng);
         isSnakePosition = std::binary_search(posSnake.begin(), posSnake.end(), foodPosition);
     } while (isSnakePosition);
 
