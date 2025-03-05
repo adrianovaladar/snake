160b0ac5825f0ff38254cf0500590996c348d2bc
Rogério Lopes <roger_rrl@hotmail.com>
2025-03-05 22:36:45 +0000

    move binary search method

--- src/Food.cpp
+++ src/Food.cpp
@@ -1,7 +1,8 @@
 #include "Food.h"
+#include "Utils.h"
+#include <algorithm>
 #include <random>
 #include <vector>
-#include <algorithm>
 
 Food::Food() : position(std::make_pair(-1, -1)), symbol('f') {}
 
@@ -17,24 +18,6 @@ void Food::setPosition(const std::pair<int, int> &p) {
     Food::position = p;
 }
 
-bool binarySearch (const std::vector<std::pair<int, int>> &posSnake, const std::pair<int, int> &foodPosition) {
-    int begin = 0;
-    auto end = static_cast<int>(posSnake.size()) - 1;
-
-    while (begin <= end) {
-        const auto middle = std::midpoint(begin, end);// begin + (end - begin) / 2;
-        if (foodPosition == posSnake.at(middle)) {
-            return  true;
-        }
-        if (foodPosition < posSnake.at(middle)) {
-            end = middle-1;
-        } else {
-            begin = middle+1;
-        }
-    }
-    return false;
-}
-
 void Food::setPosition(const std::pair<int, int> &sizeMap, const std::vector<std::pair<int, int>> &positionsSnake, const std::pair<int, int> &otherFoodPosition) {
     std::pair<int, int> foodPosition;
     std::vector<std::pair<int, int>> posSnake = positionsSnake;
@@ -47,7 +30,7 @@ void Food::setPosition(const std::pair<int, int> &sizeMap, const std::vector<std
         std::uniform_int_distribution distY(0, sizeMap.second - 1);
         foodPosition.first = distX(rng);
         foodPosition.second = distY(rng);
-        isSnakePosition = binarySearch(posSnake, foodPosition);
+        isSnakePosition = Utils::binarySearch(posSnake, foodPosition);
     } while (isSnakePosition || otherFoodPosition == foodPosition);
 
     this->position = foodPosition;
